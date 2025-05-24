1eb51e6dc8756f2837d20440500c0c4e97ceaad4
Adi Yohanes <adiyohanes19@gmail.com>
2025-05-24 09:17:59 +0700

    edit next

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
-  output: 'export',
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
